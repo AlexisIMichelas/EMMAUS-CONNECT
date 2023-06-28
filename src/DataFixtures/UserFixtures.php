21f5d7cd40da130bfee96c04a29365cd50f2e7a0
fliewight <lissjulien@gmail.com>
2023-06-28 20:39:57 +0200

    Save modifications US_User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('user@emmaus-connect.org');
+        $user->setRoles(['ROLE_USER']);
+        $hashedPassword = $this->passwordHasher->hashPassword($user, 'user');
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setEmail('admin@emmaus-connect.org');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $this->passwordHasher->hashPassword($admin, 'admin');
+        $admin->setPassword($hashedPassword);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
