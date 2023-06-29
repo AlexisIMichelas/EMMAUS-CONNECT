608e61e79edd65e1f10b71d0d434c889c10e6db1
virginie lebleu <virginie.lebleu59@gmail.com>
2023-06-29 16:10:57 +0200

    ajustement formulaire

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -11,25 +11,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class FormController extends AbstractController
 {
     #[Route('/questionnaire', name: 'show')]
-    public function show(Request $request, CharacterRepository $characterRepository): Response
-    {
-        $character = new Character();
-        $form = $this->createForm(CharacterType::class, $character);
-        $form->handleRequest($request);
+public function show( Request $request, CharacterRepository $characterRepository): Response
+{
+    $character = new Character();
+    $form = $this->createForm(CharacterType::class, $character);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $characterRepository->save($character, true);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $characterRepository->save($character, true);
 
-            return $this->redirectToRoute('app_questionnaire_index');
-        }
-
-        return $this->render('questionnaire/show.html.twig', [
-        'character' => $character,
-        'form' => $form,
-
-        ]);
+      return $this->redirectToRoute('app_questionnaire_index');
     }
+
+  return $this->render('questionnaire/show.html.twig', [
+    'character' => $character,
+    'form' => $form,
+
+  ]);
+}
 }
